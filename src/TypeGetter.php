f048c1829ff29fc3828884a9779bf01601d78d3c
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 12:39:54 +0200

    Applied fixes from StyleCI (#1)
    [ci skip] [skip ci]

--- src/TypeGetter.php
+++ src/TypeGetter.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of JSON Mapper Model Generator.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\JsonMapper;
 
 class TypeGetter
@@ -33,7 +42,7 @@ class TypeGetter
      */
     private static function isDateTime($data)
     {
-        if (!is_string($data)) {
+        if (! is_string($data)) {
             return false;
         }
 
